a2d2a18b2a7b97a0fee95cdbc1578aa2fb4e2127
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-01-06 14:30:05 +0100

    controleur affiche tous les billets  dans admin
    controleur affiche tous les billets  dans admin

--- admin.php
+++ admin.php
@@ -1,120 +1,49 @@
-<!DOCTYPE html>
- <html>
+<?php 
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="robots" content="noindex,nofollow">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Description" content="blog ecrivain avec articles et commentaires.">
-    <meta name="Keywords" content="ecrivain blog">
-     <link rel="stylesheet" type="text/css" href="styles.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-    <title>blog ecrivain</title>
-  </head>
+$db= new PDO('mysql:host=localhost;dbname=news','root','');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  <body>
-  
-    <div id="global">
-       <?php include ("adheader.php");?>
-       
-       <!-- ajout d article -->
-       <section class="un">
+require_once 'classbillet.php';
+require_once 'classcomment.php';
+require_once 'manager.php';
 
-                <?php if (isset($message)){echo $message;} ?>
+$manager= new Managerbc($db);
 
-       
-           <div>
-           
-                <form action="modele.php" method="post" id="formbillet">
-                
-                      <fieldset>
-                       
-                         <legend>AJOUTER ARTICLE</legend>
-                      
-                            <p>
-                               <label>
-                                      Titre:
-                               </label>
-                                  <input type="text" value=" " required="required" name="titreb" pattern="[a-zA-Z]">
-                                  
-                            </p>
-                            
-                      
-                            <p>
-                               <label>
-                                      Auteur:
-                               </label>
-                                  <input type="text" value="" required="required" name="auteurb" pattern="[a-zA-Z]">
-                                  
-                            </p>
-                            
-                      
-                            <p>
-                               <label>
-                                      Article:
-                               </label>
-                                  <textarea rows="" cols="8" required="required" name="messageb" id="messageb"> </textarea>
-                                  
-                           </p>
-                                 
-                          <p>
-                             <input type="submit" value="ajouter" name="updb">
-                          </p>
-                          
-                      </fieldset>                     
-                      
-                </form>
-                
-           </div>
-           
-       </section>
-       
-       <!-- affichage de tous les billets -->       
-       <section class="un">
-       
-           <div class="cadrebillet">
-             <?php foreach ($billets as $billet): ?>
-             
-                <h3><?= $billet->titreb() ?> </h3>
+//LISTER TOUS LES BILLETS
 
-                <time><?= $billet->dateb() ?> </time>
-
-                <h6><?= $billet->auteurb() ?> </h6>
-
-                <p><?= $billet->messageb() ?> </p>
-
-                <table class="tablebillet">
-                  <tr>
-                      <td>
-                          <a href="controler.php?idbillet=<?= $billet->idb() ?>">Gerer article et commentaires</a>
-                      </td>
-                  </tr>
-                  <tr>
-                      <td>
-                          <a href="controler.php?delb=<?= $billet->idb() ?>">Supprimer article</a>
-                      </td>
-                  </tr>
-                  <tr>
-                      <td>
-                          <a href="controler.php?idbillet=<?= $billet->idb() ?>">Modifier article</a>
-                      </td>
-                  </tr>
-                </table>
-                
-                <hr>
-             
-             <?php endforeach; ?>
-
-           </div> 
-                 
-       </section>
+$billets=$manager->getListb();
  
-       <?php include ("adfooter.php");?>
-             
-    </div>
-    
-    <script type="text/javascript" src="mce.js"></script>
-  </body>
+//AJOUT BILLET
 
- </html>
+if (isset($_POST['addb']) && isset($_POST['auteurb'])){
+	$billet= new Billet([
+			'auteurb'=>$_POST['auteurb'],
+			'titreb'=>$_POST['titreb'],
+			'messageb'=>$_POST['messageb'], 
+	]);
+
+	if ($billet->validb()){
+		$res =$manager->addb($billet);
+      $message='Article ajouté!';
+		//if($res){ $message='Article ajouté!';}
+		
+	}
+	else {$message="echec ajout article!";
+	      $erreurs=$billet->erreurslistb();
+	     }
+	
+}
+
+//SUPPRIMER BILLET
+
+if (isset($_GET['delb'])){
+	$delb=(int)$_GET['delb'];
+	
+	$manager->deleteb($delb);
+	$manager->deleteListc($delb);
+	
+	$message='billet supprimé!';
+	
+}
+
+require 'adminvue.php';
