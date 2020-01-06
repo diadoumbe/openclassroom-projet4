1896810ab813b707683ead0716f40f52f12388a9
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-01-06 14:38:00 +0100

    controleurtous les billets cotes site web
     page principale du site web affiche tous les billets cotes site web

--- index.php
+++ index.php
@@ -1,67 +1,19 @@
-<!DOCTYPE html>
- <html>
+<?php 
 
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Description" content="blog ecrivain avec articles et commentaires.">
-    <meta name="Keywords" content="ecrivain blog">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <title>blog ecrivain</title>
-  </head>
+$db= new PDO('mysql:host=localhost;dbname=news','root','');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  <body>
-  
-    <div id="global">
-       <?php include ("header.php");?>
-       
-       <?php include ("poster.php");?>
+require_once 'classbillet.php';
+require_once 'classcomment.php';
+require_once 'manager.php';
 
-       <!-- affichage de tous les billets -->       
-       <section class="un">
-       
-           <div class="cadrebillet">
-             <?php foreach ($billets as $billet): ?>
-             
-                <h3><?= $billet->titreb() ?> </h3>
+$manager= new Managerbc($db);
 
-                <time><?= $billet->dateb() ?> </time>
+//LISTER TOUS LES BILLETS
 
-                <h6><?= $billet->auteurb() ?> </h6>
 
-                <p><?= $billet->messageb() ?> </p>
+$billets=$manager->getListb();
 
-                <table class="tablebillet">
-                  <tr>
-                      <td>
-                          <a href="controler.php?idbillet=<?= $billet->idb() ?>">Voir Article et commentaires</a>
-                      </td>
-                  </tr>
-                  <tr>
-                      <td>
-                          <a href="controler.php?idbillet=<?= $billet->idb() ?>">Ajoutez un commentaire</a>
-                      </td>
-                  </tr>
-                </table>
-                
-                <hr>
-             
-             <?php endforeach; ?>
-
-           </div> 
-                 
-       </section>
- 
-       <?php include ("footer.php");?>
-             
-    </div>
+    require 'indexvue.php';
     
-  </body>
 
- </html>
