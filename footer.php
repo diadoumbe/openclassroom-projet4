288dc12e6090da3fcdc05ec5bcae61c021569ff0
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-01-06 14:47:43 +0100

    footer site web
    footer site web

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer>
+
+  <nav>
+    <ul>
+      <li>
+         <a></a>      
+      </li>
+      <li>
+         <a></a>      
+      </li>    
+      <li>
+         <a></a>      
+      </li>    
+          
+    </ul>  
+  </nav>
+
+</footer>
