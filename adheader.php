2f93dd00bdec81809572aa63bbd66de9f621a503
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-01-06 14:47:08 +0100

    header admin
    header admin

---
+++ adheader.php
@@ -0,0 +1,36 @@
+<header>
+
+  <div id="headerlogo">
+
+   <div> <img alt="logo ecrivain" src="image/logoecrivain.png" width="120"></div>
+     <div>
+       <span id="titreroman"><strong>PEUR SUR LE NIL</strong></span>
+       <br>
+       <span id="auteurtitre"><em>par Edmond Millis</em></span>
+     </div>
+  </div>
+
+
+  <nav>
+
+    <ul>
+        <li>
+            <a href="admin.php">Accueil administration</a>
+        </li>
+        <li>
+            <a href="admin.php">Tous les articles</a>
+        </li>
+        <li>
+            <a href="admin.php">Ajouter article</a>
+        </li>
+        <li>
+            <a href="adsignal.php">Commentaires signales</a>
+        </li>
+            
+    
+    
+    </ul>  
+  
+  </nav>
+</header>
+
