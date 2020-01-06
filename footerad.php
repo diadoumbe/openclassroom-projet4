733bcb08ba45b0c3d1d15d1c55314667e1a2d4c0
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-01-06 14:49:43 +0100

    footer admin
    footer admin

---
+++ footerad.php
@@ -0,0 +1,19 @@
+<footer>
+
+  <nav>
+    <ul>
+      <li>
+         <a></a>      
+      </li>
+      <li>
+         <a></a>      
+      </li>    
+      <li>
+         <a></a>      
+      </li>    
+          
+    </ul>  
+  </nav>
+
+</footer>
+
