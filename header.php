bf8c6e94a21cdf84ab5a199bfbefccf6e6d2f944
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-01-06 14:46:26 +0100

    header site web
    header site web

---
+++ header.php
@@ -0,0 +1,23 @@
+<header>
+
+  <div id="headerlogo">
+
+   <div> <img alt="logo ecrivain" src="image/logoecrivain.png" width="120"></div>
+     <div>
+       <span id="titreroman"><strong>PEUR SUR LE NIL</strong></span>
+       <br>
+       <span id="auteurtitre"><em>par Edmond Millis</em></span>
+     </div>
+  </div>
+
+  <nav>
+
+    <ul>
+        <li>
+            <a href="admin.php">Connexion<br> administration</a>
+        </li>
+        
+    </ul>  
+  
+  </nav>
+</header>
