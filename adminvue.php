82b41fca7a92d58834afbbabdce8d20166c971c2
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-02-18 17:49:02 +0100

    vue de l accueil administration
    vue de l accueil administration

--- adminvue.php
+++ adminvue.php
@@ -1,31 +1,18 @@
-<!DOCTYPE html>
- <html>
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="robots" content="noindex,nofollow">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Description" content="ouvrage peur sur le nil par edmond millis.">
-    <meta name="Keywords" content="roman edmond millis">
-     <link rel="stylesheet" type="text/css" href="style.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-    <title>Peur sur le nil,par edmond milis</title>
-  </head>
+<?php $titre="Accueil administration";?>
 
-  <body>
-  
-    <div id="global">
-       <?php include ("adheader.php");?>
-       
-       <!-- ajout d article -->
+<?php ob_start(); ?>
+
+      <!-- ajout d article -->
        <section class="un">
-
-                <?php if (isset($message)){echo $message;} ?>
+           
+           <h3 class="titrerubrique">Ajout article:</h3>
+           
+                <?php if (!empty($_GET['message'])){echo'<h3 class="actionok">'.htmlspecialchars($_GET['message']).'</h3> ';} ?>
        
            <div class="cadrebillet">
            
-                <form action="admin.php" method="post" id="formbillet">
+                <form action="./admin.php?action=addbillet" method="post" id="formbillet">
                 
                       <fieldset>
                        
@@ -35,7 +22,7 @@
                                <label>
                                       Titre:
                                </label>
-                                  <input type="text" required="required" name="titreb" pattern="[a-zA-Z _-]{1,50}">
+                                  <input type="text" required="required" name="titreb" pattern="[a-zA-Z _-]{1,50}" title="titre,50lettres max ex:a-z0-9">
                                   
                             </p>
                             
@@ -44,7 +31,7 @@
                                <label>
                                       Auteur:
                                </label>
-                                  <input type="text" required="required" name="auteurb" pattern="[a-zA-Z _-]{1,30}">
+                                  <input type="text" required="required" name="auteurb" pattern="[a-zA-Z _-]{1,50}" title="auteur,50lettres max ex:a-z0-9">
                                   
                             </p>
                             
@@ -71,36 +58,44 @@
        
        <!-- affichage de tous les billets -->       
        <section class="un">
-       
-        <?php if (empty($billets)){echo "AUCUN ARTICLE PUBLIE!";
-         }else {?>
+             
+             <?php if (!empty($billets)){?>
+             <h3 class="titrerubrique">Articles:</h3>
+            <?php }?>
+            
+            <?php if (!empty($message)){echo'<h3 class="actionok">'.htmlspecialchars($message).'</h3> ';} ?>
+             
+           <?php if (empty($billets)){echo "AUCUN ARTICLE PUBLIE!";
+              }else {?>                            
                   
              <?php foreach ($billets as $billet): ?>
              
              <div class="cadrebillet">
              
-                <h3><strong><?= $billet->titreb() ?> </strong></h3>
+                <h2>
+                    <strong><?= htmlspecialchars($billet->titreb()) ?> </strong>
+                </h2>                
 
-                <time><i><?= $billet->dateb() ?> </i></time>
+                <h4>
+                    <time><i>Edite le: <?= $billet->dateb() ?> </i></time>
+                    &nbsp;&nbsp;&nbsp;
+                    auteur: <em><?= htmlspecialchars($billet->auteurb()) ?> </em>
+               </h4>
 
-                <h6><em><?= $billet->auteurb() ?> </em></h6>
-
-                <p class="textuel"><?=nl2br($billet->messageb())  ?> </p><p>
+                <div class="textuel"><?= $billet->messageb()  ?> </div>
 
                 <table class="tablebillet">
                   <tr>
                       <td>
-                          <a href="adbillet.php?idbillet=<?php echo $billet->idb(); ?> " >Gerer article et commentaires</a>
+                          <a href="./admin.php?action=billetad&idbillet=<?php echo $billet->idb(); ?> " >Gerer article et commentaires</a>
                       </td>
-                  </tr>
-                  <tr>
+                 
                       <td>
-                          <a href="admin.php?delb=<?php echo $billet->idb(); ?> " >Supprimer article</a>
+                          <a href="./admin.php?action=deladminb&delb=<?php echo $billet->idb(); ?> " >Supprimer article</a>
                       </td>
-                  </tr>
-                  <tr>
+                  
                       <td>
-                          <a href="adbillet.php?idbillet= <?php echo $billet->idb(); ?> " >Modifier article</a>
+                          <a href="./admin.php?action=billetad&idbillet=<?php echo $billet->idb(); ?> " >Modifier article</a>
                       </td>
                   </tr>
                 </table>                                                         
@@ -111,14 +106,10 @@
             
           <?php }?>
                  
-       </section>
- 
-       <?php include ("footerad.php");?>
-             
-    </div>
-    
-    <script type="text/javascript" src="mce.js"></script>
-  </body>
+       </section>       
+<?php $contenu= ob_get_clean(); ?>
 
- </html>
+<?php require 'template2.php'; ?>
+
+
 
