68dea8e603551369c6ca9f32f82c3e345ab5db99
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-02-18 17:47:51 +0100

    routeur cotes administration
    routeur de la partie administration du site

--- admin.php
+++ admin.php
@@ -1,49 +1,126 @@
-<?php 
+<?php
+require 'controler/controler2.php';
 
-$db= new PDO('mysql:host=localhost;dbname=news','root','');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-require_once 'classbillet.php';
-require_once 'classcomment.php';
-require_once 'manager.php';
-
-$manager= new Managerbc($db);
+// admin,deladminb     billetad,updbilletad,delbilletad,delcbilletad     signalad,signalcdel,listsignalc
 
 //LISTER TOUS LES BILLETS
 
-$billets=$manager->getListb();
- 
+if (isset($_GET['action']))
+{
+    if ($_GET['action']=='admin')
+    {
+    	listbillets($manager);
+    }
+}
+
+
 //AJOUT BILLET
 
-if (isset($_POST['addb']) && isset($_POST['auteurb'])){
-	$billet= new Billet([
-			'auteurb'=>$_POST['auteurb'],
-			'titreb'=>$_POST['titreb'],
-			'messageb'=>$_POST['messageb'], 
-	]);
-
-	if ($billet->validb()){
-		$res =$manager->addb($billet);
-      $message='Article ajouté!';
-		//if($res){ $message='Article ajouté!';}
-		
+if (isset($_GET['action']))
+{
+	if ($_GET['action']=='addbillet')
+	{
+		if ( isset($_POST['addb']) && !empty($_POST['auteurb']) && !empty($_POST['titreb']) && !empty($_POST['messageb']))
+		{
+			addbillet($manager,$_POST['auteurb'],$_POST['titreb'],$_POST['messageb']);
+		}
 	}
-	else {$message="echec ajout article!";
-	      $erreurs=$billet->erreurslistb();
-	     }
-	
 }
+
 
 //SUPPRIMER BILLET
 
-if (isset($_GET['delb'])){
-	$delb=(int)$_GET['delb'];
-	
-	$manager->deleteb($delb);
-	$manager->deleteListc($delb);
-	
-	$message='billet supprimé!';
-	
+if (isset($_GET['action']) && isset($_GET['delb']))
+{
+	if ($_GET['action']=='deladminb')
+	{
+		delbilletadmin($manager,$_GET['delb']);
+	}
 }
 
-require 'adminvue.php';
+
+//AFFICHER BILLET ET SES COMMENTAIRES
+
+if (isset($_GET['action']) && isset($_GET['idbillet']))
+{
+	if ($_GET['action']=='billetad')
+	{
+		//$idb=$_GET['idbillet'];
+		
+		billetad($manager,$_GET['idbillet']);
+	}
+}
+
+
+//MODIFIER BILLET
+
+if (isset($_GET['action']) /*&& isset($_GET['idbillet'])*/)
+{
+	if ($_GET['action']=='updbillet')
+	{
+		if (isset($_POST['updb']) && !empty($_POST['idb']) && !empty($_POST['auteurb']) && !empty($_POST['titreb']) && !empty($_POST['messageb']) )
+		{
+			updbilletad($manager,$_POST['idb'],$_POST['auteurb'],$_POST['titreb'],$_POST['messageb']);
+			
+			unset($_POST);
+		}
+	}
+}
+
+
+
+
+//SUPPRIMER BILLET
+
+if (isset($_GET['action']) && isset($_GET['delb']))
+{
+	if ($_GET['action']=='delbilletad')
+	{
+		delbilletad($manager,$_GET['delb']);
+	}
+}
+
+
+//SUPPRIMER UN COMMENTAIRE
+
+if (isset($_GET['action']) && isset($_GET['delc']) && isset($_GET['delidb']))
+{
+	if ($_GET['action']=='delcbilletad')
+	{
+		delcbilletad($manager,$_GET['delc'],$_GET['delidb']);
+	}
+}
+
+
+//LISTER TOUS LES COMMENTAIRES SIGNALES
+
+if (isset($_GET['action']))
+{
+	if ($_GET['action']=='listsignalc')
+	{
+		listsignalc($manager);
+	}
+}
+
+
+//AFFICHER lE BILLET ET LE COMMENTAIRE SIGNALE
+
+if (isset($_GET['action']) && isset($_GET['idbilletsignal']) && isset($_GET['idcomsignal']))
+{
+	if ($_GET['action']=='signalad')
+	{
+		signalc($manager,$_GET['idbilletsignal'],$_GET['idcomsignal']);
+	}
+}
+
+
+//SUPPRIMER UN COMMENTAIRE
+
+if (isset($_GET['action']) && isset($_GET['delc']))
+{
+	if ($_GET['action']=='signalcdel')
+	{
+		signalcdel($manager,$_GET['delc']);
+	}
+}
+
