23d0d4f1e92600c281e59cf416e548a1a398438a
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-02-18 17:40:18 +0100

    routeur et page principale du site

--- index.php
+++ index.php
@@ -1,19 +1,77 @@
-<?php 
+<?php
 
-$db= new PDO('mysql:host=localhost;dbname=news','root','');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require './controler/controler1.php';
 
-require_once 'classbillet.php';
-require_once 'classcomment.php';
-require_once 'manager.php';
-
-$manager= new Managerbc($db);
+// index   billet,signalcom
 
 //LISTER TOUS LES BILLETS
 
+if (isset($_GET['action']))
+{
+	if ($_GET['action']=='index')
+	{
+		listbillets($manager);
+		//require 'vue/indexvue.php';
+	}
+ 
+}
 
-$billets=$manager->getListb();
 
-    require 'indexvue.php';
-    
+//AFFICHER BILLET ET SES COMMENTAIRES
 
+if (/*isset($_GET['idbillet']) &&*/ isset($_GET['action']) )
+{
+    if ($_GET['action']=='billet')
+       {
+    	if (isset($_GET['idbillet']))
+    	  {
+    	  	$idb=(int)$_GET['idbillet'];
+    	  	billet($manager,$idb);
+    	  	
+    	  	//require 'vue/billetvue.php';
+    	  }
+    	  
+       }
+       
+}
+
+
+//AJOUT COMMENTAIRE
+
+if ( isset($_GET['action']) )
+{
+	if ($_GET['action']=='addcomment')
+	{
+		if (isset($_POST['addc']) && isset($_POST['idbc']) && isset($_POST['auteurc']) && isset($_POST['titrec']) && isset($_POST['commentc']) )
+		{
+			addcomment($manager,$_POST['idbc'],$_POST['auteurc'],$_POST['titrec'],$_POST['commentc']);
+			
+			unset($_POST);
+		
+		}
+		 
+	}
+	 
+}
+
+
+
+//SIGNALER COMMENTAIRE
+
+if (isset($_GET['action']) && isset($_GET['signalc']) && isset($_GET['signaldb']))
+{
+    if ($_GET['action']=='signalcom')
+    {
+     signalcomment($manager);
+     
+     //require 'vue/billetvue.php';
+    }
+}
+
+
+//action par defaut pour la page index 
+
+if ( !isset($_GET['action']) )
+{
+	listbillets($manager);
+}
