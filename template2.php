fff228c3afbcec6f15b725c2c9a17d13a5337965
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-02-18 17:52:29 +0100

    vue template administration
    vue template cotes administration

---
+++ template2.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+ <html>
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description" content="ouvrage Billet simple pour l'alaska par jean Forteroche.">
+    <meta name="Keywords" content="roman jean forteroche">
+     <link rel="stylesheet" type="text/css" href="./../contenu/style.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <title>Billet simple pour l'alaska, par jean Forteroche</title>
+  </head>
+
+  <body>
+  
+    <div id="global">
+       
+       <header>
+
+  <div id="headerlogo">
+
+       <div> 
+           <img alt="logo ecrivain" src="./../contenu/image/logoecrivain.png" width="120"></div>
+       <div>
+       
+       <span id="titreroman"><strong>BILLET SIMPLE POUR L'ALASKA</strong></span>
+       <br>
+       <span id="auteurtitre"><em>par jean Forteroche</em></span>
+     </div>
+  </div>
+
+
+  <nav>
+
+    <ul>
+        <li>
+            <a href="./admin.php?action=admin">Accueil administration</a>
+        </li>
+        <li>
+            <a href="./admin.php?action=admin">Tous les articles</a>
+        </li>
+        <li>
+            <a href="./admin.php?action=admin">Ajouter article</a>
+        </li>
+        <li>
+            <a href="./admin.php?action=listsignalc">Commentaires signales</a>
+        </li>
+        <li>
+            <a href="./../index.php?action=index">Accueil site</a>
+        </li>    
+    
+    
+    </ul>  
+  
+  </nav>
+</header>
+       
+       <section class="rubrique">
+       
+                 <h3><?= $titre ?></h3>
+       </section>
+       
+      <div id="contenu">
+      
+            <?= $contenu ?>
+      
+      </div>
+ 
+       <footer>
+
+  <div>
+     <img alt="billet simple pour l'alaska" src="./../contenu/image/ecrivain2.jpg">
+  </div>
+   
+   <nav>
+
+    <ul>
+        <li>
+            <a href="./admin.php?action=admin">Accueil administration</a>
+        </li>
+        
+        <li>
+            <a href="./admin.php?action=admin">Tous les articles</a>
+        </li>
+        
+        <li>
+            <a href="./admin.php?action=admin">Ajouter article</a>
+        </li>
+        
+        <li>
+            <a href="./admin.php?action=listsignalc">Commentaires signales</a>
+        </li>
+        
+        <li>
+            <a href="./../index.php?action=index">Accueil site</a>
+        </li>    
+        
+    </ul>  
+  
+  </nav>
+
+</footer>
+             
+    </div>
+    
+    <script type="text/javascript" src="./js/tinymce_3.5.12/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
+    <script type="text/javascript" src="./js/mce.js"></script>
+    <script type="text/javascript" src="./js/inittinymce.js"></script>
+  </body>
+
+ </html>
+
+
