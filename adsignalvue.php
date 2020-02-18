0f4b82088c66780d085cf5bc7c0802186214f6af
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-02-18 17:51:23 +0100

    vue commentaires signales administration
    vue de tous les commentaires signales cotes administration

--- adsignalvue.php
+++ adsignalvue.php
@@ -1,53 +1,42 @@
-<!DOCTYPE html>
- <html>
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="robots" content="noindex,nofollow">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Description" content="ouvrage peur sur le nil par edmond millis.">
-    <meta name="Keywords" content="roman edmond millis">
-     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-    <script src="https://kit.fontawesome.com/bf2f2c9ab5.js" crossorigin="anonymous"></script>
-     <link rel="stylesheet" type="text/css" href="style.css">   
-    <title>Peur sur le nil,par edmond milis</title>
-  </head>
+<?php $titre="Commentaires signales administration";?>
 
-  <body>
-  
-    <div id="global">
-    
-       <?php include ("adheader.php");?>
+<?php ob_start(); ?>
 
-    
        <!--  affichage article -->
        <section class="un" >
-
-                <?php if (isset($message)){echo $message;} ?> 
+           
+           <?php if (isset($billet)){?>
+           <h3 class="titrerubrique">Article:</h3>
+          <?php }?>
+           
+                <?php if (!empty($message)){echo'<h3 class="actionok">'.htmlspecialchars($message).'</h3> ';} ?> 
                 
-               <?php if (empty($billet)){echo "AUCUN BILLET A AFFICHER!";
-               }else {?> 
+               <?php if (!empty($billet)){ ?> 
                      
            <div class="cadrebillet">           
-                            <h3>
-                             <strong>
-                                  <?= $billet->titreb()  ?>                                 
-                             </strong>
-                            </h3>                                                 
-                            <h6>
-                             <em>
-                                  <?= $billet->auteurb()  ?>                                  
-                             </em>
-                            </h6>                                                  
-                            <time>
-                              <i>
-                                Date: <?= $billet->dateb()  ?>                                  
-                             </i>
-                            </time>                                                  
-                            <p class="textuel">
-                              ><?= nl2br( $billet->messageb())?>                                  
-                            </p>                
+                            <h2>
+                                <strong>
+                                       <?= htmlspecialchars($billet->titreb())  ?>                                 
+                               </strong>
+                            </h2> 
+                                                                            
+                            <h4>
+                                <time>
+                                 <i>
+                                    Edite le: <?= $billet->dateb()  ?>                                  
+                                </i>
+                               </time>
+                                &nbsp;&nbsp;&nbsp;&nbsp;
+                                auteur:
+                                <em>
+                                    <?= htmlspecialchars($billet->auteurb())  ?>                                  
+                                </em>
+                            </h4>                                                  
+                                                                             
+                            <div class="textuel">
+                              <?=  $billet->messageb() ?>                                  
+                            </div>                
            </div>
            
             <?php }?>
@@ -56,29 +45,36 @@
 
 
 <section class="un">
-
-         <?php if (isset($message1)){echo $message1;} ?>
+          
+          <?php if (!empty($comment)){?>
+          <h3 class="titrerubrique">Commentaire:</h3>
+         <?php }?>
+          
+         <?php if (!empty($message1)){echo'<h3 class="actionok">'.htmlspecialchars($message1).'</h3> ';} ?>
          
-          <?php if (empty($comment)){echo "VEUILLEZ SELECTIONNER UN COMMENTAIRE!";
-          }else {?>
+          <?php if (!empty($comment)){ ?>
          
            <div class="cadrecomment">
              
-            <?php if ($comment->signalc()==1){?> <h4 class="alert">Commentaire signalé!</h4> <?php }?>
+            <?php if ($comment->signalc()==1){?> <h3 class="alert">Commentaire signale!</h3> <?php }?>
              
-                <h3><strong><?= $comment->titrec() ?> </strong></h3>
+                <h2>
+                    <strong><?= htmlspecialchars($comment->titrec()) ?> </strong>
+                </h2>                
 
-                <time><i><?= $comment->datec() ?> </i></time>
+                <h4>
+                    <time><i>Edite le: <?= $comment->datec() ?> </i></time>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    auteur:<em><?= htmlspecialchars($comment->auteurc()) ?> </em>
+               </h4>
 
-                <h6><em><?= $comment->auteurc() ?> </em></h6>
+                <div class="textuel"><?=  htmlspecialchars($comment->commentc()) ?> </div>
 
-                <p class="textuel"><?= nl2br( $comment->commentc()) ?> </p>
-
-                <table class="tablebillet">
-                  <tr>
+                <table class="tablecomment">
+                  
                   <tr>
                       <td>
-                          <a href="adsignal.php?delc=<?php echo $comment->idc(); ?>">Supprimer commentaire</a>
+                          <a href="./admin.php?action=signalcdel&delc=<?php echo $comment->idc(); ?>">Supprimer commentaire</a>
                       </td>
                   </tr>
                 </table>
@@ -92,8 +88,12 @@
        
        <!-- affichage de tous les commentaires de l article -->       
        <section class="un">
-
-         <?php if (isset($message1bis)){echo $message1bis;} ?>
+          
+          <?php if (!empty($signals)){?>
+          <h3 class="titrerubrique">Commentaires:</h3>
+         <?php }?>
+          
+         <?php if (isset($message1bis)){echo'<h3 class="actionok">'.htmlspecialchars($message1bis).'</h3> ';} ?>
          
            <?php if (empty($signals)){echo "AUCUN COMMENTAIRES SIGNALES!";
            }else {?>
@@ -102,25 +102,28 @@
              
            <div class="cadrecomment">
              
-            <?php if ($signal->signalc()==1){?> <h4 class="alert">Commentaire signalé!</h4> <?php }?>
+            <?php if ($signal->signalc()==1){?> <h4 class="alert">Commentaire signale!</h4> <?php }?>
              
-                <h3><strong><?= $signal->titrec() ?> </strong></h3>
+                <h2>
+                    <strong><?= htmlspecialchars($signal->titrec()) ?> </strong>
+               </h2>                
 
-                <time><i><?= $signal->datec() ?> </i></time>
+                <h4>
+                     <time><i>Edite le: <?= $signal->datec() ?> </i></time>
+                     &nbsp;&nbsp;&nbsp;
+                     auteur:<em><?= htmlspecialchars($signal->auteurc()) ?> </em>
+               </h4>
 
-                <h6><em><?= $signal->auteurc() ?> </em></h6>
+                <div class="textuel"><?= htmlspecialchars( $signal->commentc()) ?> </div>
 
-                <p class="textuel"><?= nl2br( $signal->commentc()) ?> </p>
-
-                <table class="tablebillet">
+                <table class="tablecomment">
                   <tr>
                       <td>
-                          <a href="adsignal.php?idbilletsignal=<?php echo $signal->idbc(); ?>&idcomsignal=<?php echo $signal->idc(); ?>">Gerer article et commentaires</a>
+                          <a href="./admin.php?action=signalad&idbilletsignal=<?php echo $signal->idbc(); ?>&idcomsignal=<?php echo $signal->idc(); ?>">Gerer article et commentaires</a>
                       </td>
-                  </tr>
-                  <tr>
+                  
                       <td>
-                          <a href="adsignal.php?delc=<?php echo $signal->idc(); ?>">Supprimer commentaire</a>
+                          <a href="./admin.php?action=signalcdel&delc=<?php echo $signal->idc(); ?>">Supprimer commentaire</a>
                       </td>
                   </tr>
                  
@@ -132,15 +135,10 @@
                
              <?php }?>
                   
-       </section>
- 
-       <?php include ("footerad.php");?>
-             
-    </div>
-    
-  </body>
+       </section>      
+<?php $contenu= ob_get_clean(); ?>
 
- </html>
+<?php require 'template2.php'; ?>
 
 
 
