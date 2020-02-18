1cbd749c134684ca5ef44b4f8fb8cdc514cf115f
diadoumbe <58658189+diadoumbe@users.noreply.github.com>
2020-02-18 17:43:20 +0100

    vue par article
    vue de chaque article

--- billetvue.php
+++ billetvue.php
@@ -1,49 +1,41 @@
-<!DOCTYPE html>
- <html>
 
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Description" content="ouvrage peur sur le nil par edmond millis.">
-    <meta name="Keywords" content="roman edmond millis">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">    
-     <link rel="stylesheet" type="text/css" href="style.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-    <title>Peur sur le nil,par edmond milis</title>
-  </head>
+<?php $titre="Article et commentaires";?>
 
-  <body>
-  
-    <div id="global">
-       <?php include ("header.php");?>
-       
-       <?php include ("poster.php");?>
+<?php ob_start(); ?>
 
-       <!-- affichage de tous les billets -->       
+<!-- affichage du billet -->       
        <section class="un">
+          
+          <?php if (!empty($billet))
+                {?> 
+                 <h3 class="titrerubrique">Article:</h3>
+          <?php }
+          else {
+          	    echo '<a href="./index.php">veuillez selectionez un article ici!</a>';
+               }?>
+           
+               <?php if (!empty($_GET['message'])){ echo '<h3 class="actionok">'.htmlspecialchars($_GET['message']).'</h3>';}?>
        
-                       <?php if (isset($message)){echo $message;} ?>
-       
-                      <?php if (empty($billet)){echo "SELECTIONNEZ UN BILLET!";
-                       }else {?>
+               <?php if (empty($billet)){echo "SELECTIONNEZ UN BILLET!";
+                                        }else {  $hideid=$billet->idb();    ?>
        
            <div class="cadrebillet">
              
-                <h3><strong><?= $billet->titreb() ?> </strong></h3>
+                <h2>
+                    <strong><?= htmlspecialchars($billet->titreb()) ?> </strong>
+                </h2>                
 
-                <time><i><?= $billet->dateb() ?> </i></time>
+                <h4>
+                    <time><i>Edite le: <?= $billet->dateb() ?> </i></time>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    auteur:<em> <?= htmlspecialchars($billet->auteurb()) ?> </em>
+               </h4>
 
-                <h6><em><?= $billet->auteurb() ?> </em></h6>
-
-                <p class="textuel"><?= $billet->messageb() ?> </p>             
+                <div class="textuel"><?= $billet->messageb() ?> </div>             
 
            </div> 
            
-           <?php }?>
+           <?php // }?>
                  
        </section>
  
@@ -51,9 +43,11 @@
        <!-- formulaire ajout commentaire -->
        <section class="un">
        
-           <div>
+           <h3 class="titrerubrique">Ajout commentaire:</h3>
+       
+           <div class="cadrecomment">
            
-                <form action="billet.php" method="post" id="formcomment">
+                <form action="./index.php?action=addcomment" method="post" id="formcomment" autocomplete="off">
                 
                       <fieldset>
                        
@@ -63,7 +57,7 @@
                                <label>
                                       Titre:
                                </label>
-                                  <input type="text" value=" " required="required" name="titrec" pattern="[a-zA-Z _-]{1,50}">
+                                  <input type="text" value="" required="required" name="titrec" id="titrec" pattern="[a-zA-Z _-]{1,50}" title="titre,50lettres max ex:a-z0-9">
                                   
                             </p>
                             
@@ -72,17 +66,18 @@
                                <label>
                                       Auteur:
                                </label>
-                                  <input type="text" value="" required="required" name="auteurc" pattern="[a-zA-Z _-]{1,30}">
+                                  <input type="text" value="" required="required" name="auteurc" id="auteurc" pattern="[a-zA-Z _-]{1,50}" title="auteur,50lettres max ex:a-z0-9">
                                   
                             </p>
                             
                       
                             <p><?php if (isset($erreurscl)&& in_array(Comment::COMMENTC_FAU, $erreursc)) echo 'COMMENTAIRE INVALIDE!'; ?>
                                
-                                  <textarea rows="" cols="8" required="required" name="messagec" id="">Redigez commentaire </textarea>
+                                  <textarea rows="5" cols="" required="required" minlength="2" maxlength="300" title="Redigez commentaire, 300 lettres maximum ex:a-z0-9 et ponctuations" name="commentc" id="messagec"> </textarea>
                                   
                             </p>
-                                 <input type="hidden" value="<?php if (!empty($billet)){ $billet->idb(); }?>" name="idbc">
+                                 <input type="hidden" value="<?php echo $hideid;?>" name="idbc">
+                                 <input type="hidden" value="" name="signalc">
 
                           <p>
                              <input type="submit" value="valider" name="addc">
@@ -95,11 +90,15 @@
            </div>
            
        </section>
-       
+        <?php }?>
        <!-- affichage de tous les commentaires du billet -->       
        <section class="un">
-
-         <?php if (isset($message1)){echo $message1;} ?> 
+             
+         <?php if (!empty($comments)){?>
+             <h3 class="titrerubrique">Commentaires:</h3>
+        <?php }?>
+             
+         <?php if (isset($message1)){echo '<h3 class="actionok">'.htmlspecialchars($message1).'</h3>';} ?> 
        
         <?php if (empty($comments)){echo "AUCUN COMMENTAIRE!";
          }else {?>        
@@ -108,20 +107,24 @@
              
              <div class="cadrecomment">
              
-            <?php if ($comment->signalc()==1){?> <h4 class="alert">Commentaire signalé!</h4> <?php }?>
+            <?php if ($comment->signalc()==1){?> <h3 class="alert">Commentaire signale!</h3> <?php }?>
              
-                <h3><strong><?= $comment->titrec() ?> </strong></h3>
+                <h2>
+                    <strong><?= htmlspecialchars($comment->titrec()) ?> </strong>
+                </h2>
+                
+                <h4>
+                    <time><i>Edite le: <?= $comment->datec() ?> </i></time>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    auteur:<em><?= htmlspecialchars($comment->auteurc()) ?> </em>
+                </h4>
 
-                <time><i><?= $comment->datec() ?> </i></time>
-
-                <h6><em><?= $comment->auteurc() ?> </em></h6>
-
-                <p class="textuel"><?= nl2br($comment->commentc()) ?> </p>
+                <div class="textuel"><?= nl2br(htmlspecialchars($comment->commentc())) ?> </div>
 
                 <table class="tablecomment">
                   <tr>
                       <td>
-                          <a href="billet.php?signalc=<?php echo $comment->idc(); ?>">Signaler commentaire</a>
+                          <a href="./index.php?action=signalcom&signalc=<?php echo $comment->idc(); ?>&signaldb=<?php echo $comment->idbc(); ?>">Signaler commentaire</a>
                       </td>
                   </tr>
                 </table>                                
@@ -133,14 +136,8 @@
           <?php }?>
                 
        </section>
- 
-       <?php include ("footer.php");?>
-             
-    </div>
-    
-  </body>
+       
 
- </html>
+<?php $contenu= ob_get_clean(); ?>
 
-
-
+<?php require 'template.php'; ?>
